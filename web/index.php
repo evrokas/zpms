42644ab1b04034dd936eaf01f7f6bdae920cb14e
Evangelos Rokas <evrokas@gmail.com>
2023-12-17 02:04:59 +0200

    fixed merge, progress with routes

--- web/index.php
+++ web/index.php
@@ -1,14 +1,6 @@
 <?php
     // Project Zeus - Patient Registration System
 
-    // var_dump( $_SERVER );
-    $debug = false;
-#    $debug = true;
-
-    $cssfiles = array();
-    $cssfiles[] = 'css/styles.css';
-    $cssfiles[] = 'css/boxicons.min.css';
-
     $rootpath = substr($_SERVER['PHP_SELF'],0,strrpos($_SERVER['PHP_SELF'],'/')+1);
     
     $info = yaml_parse_file('info.yaml');
@@ -22,20 +14,14 @@
     $handlers = explode('/', '/'.$_SERVER['QUERY_STRING']);
     $handlers[0] = $_SERVER['QUERY_STRING'];
     
-    $page = 'homepage';
-    
-    if( $debug ) {
-        echo '<pre>'; print_r( $_SERVER ); echo '</pre>';
-
-        if(isset($_POST))
-            echo "Post data: " . print_r($_POST, 1);
-        
-        echo 'Page request: ' . $page;
+    if(strlen($handlers[1])) {
+        $page = $handlers[1];
+    } else {
+        $page = 'homepage';
     }
 
-
     require('templates/header.php');
-    if(false && !isset($_SESSION['username'])) {
+    if(/*false &&*/ !isset($_SESSION['username'])) {
         // header('Location: index.php/login' );
         require('pages/login.php');
     } else {
--- web/pages/homepage.php
+++ web/pages/homepage.php
@@ -1,12 +1,5 @@
 <p>
     <?php
-    // print_r( $info );
-    echo "<pre>";
-    print_r( $handlers );
-    echo "</pre>";
-
-        // echo "Root path " . $rootpath . '<br/>';
-
         if(isset($_SESSION['username'])) {
             echo "Logged in user: " . $_SESSION['username'] . " ";
             echo '<a href="logout">Logout</a>';
@@ -14,6 +7,15 @@
     ?>
 </p>
 
+
 <p>
     This is the home page of Project Zeus
+</p>
+<br/>
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quisquam praesentium repellendus temporibus
+     eligendi ea aliquam quos corporis! Ullam omnis consectetur cupiditate quaerat. Facere impedit laborum quae 
+     voluptates aliquid porro!
+     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam consequatur voluptatum tenetur eos aliquid 
+     nulla odit, repellendus ipsa doloribus eaque eum iste placeat. Officia quos voluptatibus ut nostrum, ipsam temporibus!
 </p>
