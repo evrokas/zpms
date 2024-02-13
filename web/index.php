ec0b84c9f093ecd3ba258bd09fafe8b8a039105e
Evangelos Rokas <evrokas@gmail.com>
2024-02-13 19:30:43 +0200

    implement kernel class, remove unused kernel_decoderoute(), some improvements

--- web/index.php
+++ web/index.php
@@ -2,104 +2,54 @@
 
 require_once('../fw/bootstrap.php');
 
-    // Project Zeus - Patient Registration System
-    global $rootpath;
-    
-    $rootpath = substr($_SERVER['PHP_SELF'],0,strrpos($_SERVER['PHP_SELF'],'/')+1);
+function dump_routes() {
+    global $info;
+    foreach($info as $key => $val )
+        if($key == "routes") {
+            foreach($val as $rname => $rdest) {
+                // kernel_debug("route: " . $rname . " ==> " . $rdest . " url: " . $rdest['url'] . " Title: " . $rdest['title']);
+                if(isset($rdest['arg'])) {
+                    foreach($rdest['arg'] as $arg) {
+                        kernel_debug(" -  arg: " . $arg );
+                    }
+                }
+            }
+        }
+}
 
+
+
+    // Project Zeus - Patient Registration System
     global $info;
     $info = yaml_parse_file('info.yaml');
     // print_r( $info );
 
+    $kernel = new Kernel($_SERVER, $info);
 
-    global $footer_message;
-    $footer_message = '';
+    $router = new RouterClass( $info['routes']);
+    // print_r( $router->getAllRoutes() );
 
-
-    function dump_routes() {
-        global $info;
-        foreach($info as $key => $val )
-            if($key == "routes") {
-                foreach($val as $rname => $rdest) {
-                    // kernel_debug("route: " . $rname . " ==> " . $rdest . " url: " . $rdest['url'] . " Title: " . $rdest['title']);
-                    if(isset($rdest['arg'])) {
-                        foreach($rdest['arg'] as $arg) {
-                            kernel_debug(" -  arg: " . $arg );
-                        }
-                    }
-                }
-            }
-    }
-
-    function kernel_decoderoute( $ahndlr ) {
-        global $info;
-
-        kernel_debug("Searching for path: " . $ahndlr[0] . " in routes");
-        
-        // find route in route table
-        // traverse route urls in route table
+    $template = new ZETEMTemplate('templates/', false, true);
 
 
-        $collectroutes = [];    // collected routes table
-        
-            // $hcnt=1;        // handle counter
-            // initially collect all routes, assume that all routes are correct(!)
-        $collectroutes = $info['routes'];
-        // kernel_debug('ROUTES: '. print_r( $collectroutes, 1));
-        // iterate over each route in the table
-        foreach($info['routes'] as $routename => $routedata) {
-               kernel_debug(" - explore route : " . $routename . " with data " . print_r( $routedata, 1));
-            
-            for($hcnt=1;$hcnt<count($ahndlr);$hcnt++) {
-            // explode route url
-                if($routedata['url'][0] === '/')
-                    $routedata['url'] = trim($routedata['url'], '/');
+    $req = new RequestClass($_SERVER);
+    $handlers = $req->getQueryRoute();
 
-                $rexpld = explode('/', $routedata['url']);
-                
-                kernel_debug(" - explode: " . $routename . " > " . print_r( $rexpld, 1 ));
 
-                kernel_debug(' - - compare ' . $rexpld[ $hcnt ] . ' to ' . $ahndlr[$hcnt]);
-
-                if(preg_match( $rexpld[$hcnt], "/\{.+\}/", $matches, PREG_UNMATCHED_AS_NULL)) {
-                    kernel_debug('parameter');
-                }
-                if($rexpld[ $hcnt ] != $ahndlr[ $hcnt ]) {
-                    unset( $collectroutes[ $routename ] );  
-                    break;   // remove route from array
-                }
-            }
-        }
-
-        kernel_debug('ROUTES: '. print_r( $collectroutes, 1));
-
-        if(count($collectroutes)>0) {
-            foreach($collectroutes as $colkey => $colval) {
-                kernel_debug('route: ' . $colkey . ' idx: '. $colval['_index'] . ' url: ' . $colval['url']);
-            }
-        }
-            // kernel_debug( print_r( $collectroutes, 1 ));
-    }
-    
-    // kernel_debug('start');
-    // kernel_debug($rootpath);
-    // kernel_debug('q:/'.$_SERVER['QUERY_STRING']);
-    // kernel_debug( print_r( $info, 1) );
-
-    // dump_routes();
-
+ 
     session_start();
 
     ob_start();
     require( 'templates/headers.php' );
 
-    $handlers = explode('/', '/'.$_SERVER['QUERY_STRING']);
-    $handlers[0] = $_SERVER['QUERY_STRING'];
+    // $handlers = explode('/', '/'.$_SERVER['QUERY_STRING']);
+    // $handlers[0] = $_SERVER['QUERY_STRING'];
+    // echo "<pre>" . print_r( $_SERVER, 1 ) . "</pre>";
+
+    echo( "Method: " . $req->getMethod() . '  string: ' . $req->getQueryString() . "<br/>" );
     // print_r( $handlers );
     // $path = kernel_decoderoute( $handlers );
 
-    $router = new RouterClass( $info['routes']);
-    // print_r( $router->getAllRoutes() );
 
     print_r( 'test');
     $match = $router->matchRoute( $handlers[0] );
@@ -126,12 +76,10 @@ require_once('../fw/bootstrap.php');
     } else {
         require('pages/' . $page . '.php');
     }
-    print_r( $match);
+    print_r($match);
     $router->routerCallFunction($match);
 }
 
     include('templates/footer.php');
 
     ob_end_flush();
-
-?>
