b526447012c1dd2dae4a5a3ae1a04e8eebcc56eb
Evangelos Rokas <evrokas@gmail.com>
2024-03-12 09:38:18 +0200

    changes from iocasti

---
+++ sql/msqldump.sh
@@ -0,0 +1,12 @@
+dir=`dirname $0`
+
+user=`head -1 $dir/admin.sql | gawk '{print  $6}' - | cut -d\' -f2 -`
+password=`head -1 $dir/admin.sql | gawk '{print $9}' - | cut -d\' -f2 -`
+db=`head -2 $dir/admin.sql | gawk '/DATABASE/  { print $6 }' - | cut -d\; -f1 -`
+
+#echo user $user
+#echo pass $password
+#echo db $db
+
+mysqldump -u $user -p$password $db  $1
+
