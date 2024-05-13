af8c44a2cfb6ab17fe7fb5a0b8338409d28a8c63
Evangelos Rokas <evrokas@gmail.com>
2024-05-13 18:18:24 +0300

    moved some templates/ to framework template/ folder

--- web/user_classes.php
+++ web/user_classes.php
@@ -1,6 +1,8 @@
 <?php
 
-include('classes/bootstrap_classes.php');
+if(file_exists('classes/bootstrap_classes.php'))
+    include('classes/bootstrap_classes.php');
 
-include('ClassesEx.php');
+if(file_exists('ClassesEx.php'))
+    include('ClassesEx.php');
 
