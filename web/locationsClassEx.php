5dcb350f8c6741202c8ad3bf29879a76a87d75bd
Evangelos Rokas <evrokas@gmail.com>
2024-06-07 19:15:58 +0300

    added locationsClassEx::getbyMachineName(), forgotten from split

--- web/locationsClassEx.php
+++ web/locationsClassEx.php
@@ -36,6 +36,29 @@ class locationsClassEx extends locationsClass {
         return ($arr);    
     }
 
+    static function getbyMachineName($mname) {
+        global $AppDBConnection;
+
+        if(!$AppDBConnection->isConnected()) {
+            if(!$AppDBConnection->Connect()) {
+                echo 'Could not connect to database';
+                return (null);
+            }
+        }
+
+        $sql = "SELECT * FROM locations WHERE machinename=:machinename";
+        $st = $AppDBConnection->getConnection()->prepare( $sql );
+        $st->bindValue(":machinename", $mname, PDO::PARAM_STR);
+        $st->execute();
+        $row = $st->fetch();
+
+        if($row) {
+            $rclass = new locationsClass();
+            $rclass->loadFields( $row );
+            return $rclass;
+        } else return (null);
+    }
+
     static function getbyGUID($guid) {
         global $AppDBConnection;
 
