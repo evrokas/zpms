25ef7e87140147cc41485ba488c02ca9e2226d5f
Evangelos Rokas <evrokas@gmail.com>
2024-06-07 19:24:30 +0300

    test for __APPDIR__ and __FWDIR__ in db.php{.in}, improve table dump in sql/msqldump.sh

--- sql/msqldump.sh
+++ sql/msqldump.sh
@@ -7,6 +7,7 @@ db=`head -2 $dir/admin.sql | gawk '/DATABASE/  { print $6 }' - | cut -d\; -f1 -`
 #echo user $user
 #echo pass $password
 #echo db $db
+options='--skip-opt --skip-extended-insert'
 
-mysqldump -u $user -p$password $db $@
+mysqldump -u $user -p$password $db $options $@
 #mysqldump --defaults-extra-file=<(echo $'[client]\npassword='"$password") -u $user $db $@
