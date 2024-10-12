ca4cf5abbb93caf36110a5373705ab32d0c300c6
Evangelos Rokas <evrokas@gmail.com>
2024-10-12 15:48:05 +0300

    add ignore tables in mysqldump.sh

--- sql/msqldump.sh
+++ sql/msqldump.sh
@@ -9,5 +9,12 @@ db=`head -2 $dir/admin.sql | gawk '/DATABASE/  { print $6 }' - | cut -d\; -f1 -`
 #echo db $db
 options='--skip-opt --skip-extended-insert'
 
+ignore_tables='analytics analyt'
+
+full_ignore_table=`for temp in $ignore_tables; do echo $db.$temp; done`;
+echo "IGNORE: " $full_ignore_tables
+
+
+
 mysqldump -u $user -p$password $db $options $@
 #mysqldump --defaults-extra-file=<(echo $'[client]\npassword='"$password") -u $user $db $@
