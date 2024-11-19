f799e806ee8ca43747a5d4c327bde8cbbe7ce5c1
Evangelos Rokas <evrokas@gmail.com>
2024-11-19 12:08:03 +0200

    added web/js/textarea-autoexpand.js

---
+++ web/js/textarea-autoexapand.js
@@ -0,0 +1,25 @@
+ta=document.querySelectorAll('textarea[autoexpand]');
+
+console.log(ta);
+
+
+// add event listener when text changes
+ta.forEach((t) =>{
+    // console.log('add event listener for ',  t);
+    t.addEventListener('keyup', (el) => {
+        while(el.target.clientHeight<el.target.scrollHeight) {
+            el.target.style.height = el.target.clientHeight + 10 + 'px';
+        }
+            // console.log(el.target.clientHeight, el.target.scrollHeight);
+            
+        });
+});
+
+
+// take care of heights when page loads
+ta.forEach(el =>{
+
+    while(el.clientHeight<el.scrollHeight) {
+        el.style.height = el.clientHeight + 10 + 'px';
+    }
+});
