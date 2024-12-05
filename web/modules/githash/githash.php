dc209fc053f8f48f15b5d4cffa44f66f24650d60
Evangelos Rokas <evrokas@gmail.com>
2024-12-05 17:59:39 +0200

    githash.php: add core fw branch for more clarity

--- web/modules/githash/githash.php
+++ web/modules/githash/githash.php
@@ -13,8 +13,22 @@ class githashModule extends moduleClass {
         $hash = file_get_contents($rootdir . ".git/" . $headfile);
         $branch = explode('/', $headfile)[2];
 
+        $corefile = file_get_contents($rootdir . "/fw/.git/HEAD");
+        $corefile = explode(' ', trim($corefile))[1];
+        $corehash = file_get_contents($rootdir . "/fw/.git/" . $corefile);
+        $corebranch = explode('/', $corefile)[2];
+
+
         // error_log( "git hash directory: $branch, hash: $hash\n" );
-        return $this->renderTemplate(['branch' => $branch, 'hash' => $hash, 'db' => DB_NAME /*$GLOBALS['DATABASE']*/]);
+        return $this->renderTemplate(
+            [
+                'branch' => $branch, 
+                'hash' => $hash, 
+                'db' => DB_NAME,
+            
+                'corebranch' => $corebranch,
+                'corehash' => $corehash
+            ]);
     }
 }
 
