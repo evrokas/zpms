aef4e5502cd04fde9d179acae31dd4f8672bc656
Evangelos Rokas <evrokas@gmail.com>
2024-12-05 19:47:58 +0200

    add error reporting in site.info.yaml.in, remove intialization from index.php and move it in Kernel class

--- web/index.php
+++ web/index.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 include_once(__DIR__ . "/../config/db.php");       // load database parameters
 // require_once(__DIR__ . '/../../config/db.php');
