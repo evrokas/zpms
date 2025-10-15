210b5f8c66c6c0cdb6d47a5eba9f03ccd1c7734e
Evangelos Rokas <evrokas@gmail.com>
2025-10-15 15:08:56 +0300

    updated date in footer message

--- web/modules/copyright/copyright.php
+++ web/modules/copyright/copyright.php
@@ -4,5 +4,5 @@ function register_copyright_module() {
         global $kernel;
 
         $kernel->registerModule( new htmltextModule(__DIR__, 'copyright', 'htmltext.zetem', 
-                '&copy 2023-24 by Evangelos M. Rokas, MD' . ' | <a href="#"><i class="bx bx-cog"></i></a>'));
+                '&copy 2013-25 by Evangelos M. Rokas, MD' . ' | <a href="#"><i class="bx bx-cog"></i></a>'));
 }
