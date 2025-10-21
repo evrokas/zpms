8b24cdd899b3531b2cd8a1a1522bef491c9de3e4
Evangelos Rokas <evrokas@gmail.com>
2025-10-21 09:02:37 +0300

    progress on invoices,operations,payments

--- web/modules/copyright/copyright.php
+++ web/modules/copyright/copyright.php
@@ -3,6 +3,10 @@
 function register_copyright_module() {
         global $kernel;
 
-        $kernel->registerModule( new htmltextModule(__DIR__, 'copyright', 'htmltext.zetem', 
-                '&copy 2013-25 by Evangelos M. Rokas, MD' . ' | <a href="#"><i class="bx bx-cog"></i></a>'));
+//        $kernel->registerModule( new htmltextModule(__DIR__, 'copyright', 'htmltext.zetem', 
+//                '&copy 2013-25 by Evangelos M. Rokas, MD' . ' | <a href="#"><i class="bx bx-cog"></i></a>'));
+//        $kernel->registerModule( new htmltextModule(__DIR__, 'copyright', 'htmltext.zetem', 
+//                '&copy 2013-25 by Evangelos M. Rokas, MD'));
+        $kernel->registerModule( new htmltextModule(__DIR__, 'copyright', 'copyright.zetem', 
+                '&copy 2013-25 by Evangelos M. Rokas, MD'));
 }
